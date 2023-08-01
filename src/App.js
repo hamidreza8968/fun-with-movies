aff1b9adfb2ce16274b2b0fdf6be857b7ed389d0
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-08-01 17:42:01 +0200

    add effect for storing "watched movies" into local storage.

--- src/App.js
+++ src/App.js
@@ -61,12 +61,17 @@ export default function App() {
     const [query, setQuery] = useState("");
 
     const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
 
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
 
     const [selectedId, setSelectedId] = useState(null);
+
+    // const [watched, setWatched] = useState([]);
+    const [watched, setWatched] = useState(function () {
+        const storedValue = localStorage.getItem("watched");
+        return JSON.parse(storedValue);
+    });
 
 
     // useEffect(function () {
@@ -93,11 +98,18 @@ export default function App() {
 
     function handleAddWatch(movie) {
         setWatched(watched => [...watched, movie]);
+
+        // localStorage.setItem("watched" , JSON.stringify([...watched , movie]));
     }
 
     function handleDeleteWatched(id){
         setWatched(watched => watched.filter(movie => movie.imdbID !== id));
     }
+
+
+    useEffect(function () {
+        localStorage.setItem("watched" , JSON.stringify(watched));
+    } , [watched]);
 
 
     useEffect(function () {
